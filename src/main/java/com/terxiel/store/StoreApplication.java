79497723b7eeea169d8d839008c05a93354eb93b
terfiel-22 <dietherfiel@gmail.com>
2026-06-17 23:16:47 +0800

    Using repositories

--- src/main/java/com/terxiel/store/StoreApplication.java
+++ src/main/java/com/terxiel/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.terxiel.store;
 
+import com.terxiel.store.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,5 +10,8 @@ public class StoreApplication {
 
     static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(StoreApplication.class, args);
+
+        var userRepository = applicationContext.getBean(UserRepository.class);
+        userRepository.deleteById(1L);
     }
 }
