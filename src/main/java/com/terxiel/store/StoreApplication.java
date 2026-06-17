6f57781fa53eee2d44afffbf6f200d20649fce5e
terfiel-22 <dietherfiel@gmail.com>
2026-06-17 22:23:20 +0800

    minor update

--- src/main/java/com/terxiel/store/StoreApplication.java
+++ src/main/java/com/terxiel/store/StoreApplication.java
@@ -1,26 +1,13 @@
 package com.terxiel.store;
 
-import com.terxiel.store.entities.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class StoreApplication {
 
     static void main(String[] args) {
-//        ConfigurableApplicationContext applicationContext = SpringApplication.run(StoreApplication.class, args);
-
-        var product = Product.builder()
-                .name("Red Dress")
-                .price(BigDecimal.valueOf(200.50D))
-                .build();
-
-        product.addCategory("Clothes");
-
-        System.out.println(product);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(StoreApplication.class, args);
     }
 }
