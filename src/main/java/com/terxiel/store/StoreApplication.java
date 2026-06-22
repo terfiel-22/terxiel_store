9ff1b4ec764fd441aff938e3622f5a72f303cef9
terfiel-22 <dietherfiel@gmail.com>
2026-06-22 16:53:00 +0800

    Add HomeController, Setup Thymeleaf

--- src/main/java/com/terxiel/store/StoreApplication.java
+++ src/main/java/com/terxiel/store/StoreApplication.java
@@ -1,17 +1,12 @@
 package com.terxiel.store;
 
-import com.terxiel.store.services.ProductService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(StoreApplication.class, args);
-
-        var service = applicationContext.getBean(ProductService.class);
-        service.findProductsBySpecifications(null,null,null,"Smartphone");
+        SpringApplication.run(StoreApplication.class, args);
     }
 }
---
+++ src/main/java/com/terxiel/store/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.terxiel.store.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(Model model)
+    {
+        model.addAttribute("name","SpringBoot");
+        return "index";
+    }
+}
