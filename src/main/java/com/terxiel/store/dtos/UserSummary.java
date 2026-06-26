0f2b767346de10103a53d7ead4e1ae93bf747052
terfiel-22 <dietherfiel@gmail.com>
2026-06-26 14:01:58 +0800

    Customizing Response Data

--- src/main/java/com/terxiel/store/dtos/UserSummary.java
+++ src/main/java/com/terxiel/store/dtos/UserSummary.java
@@ -1,8 +1,26 @@
 package com.terxiel.store.dtos;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDateTime;
+
 /**
  * Projection for {@link com.terxiel.store.entities.User}
  */
-public record UserSummary(Long id, String email, String name) {
+public record UserSummary(
+        @JsonIgnore
+        Long id,
+
+        String email,
+
+        @JsonProperty("full_name")
+        String name,
+
+        @JsonFormat(pattern = "MMMM d, yyyy, h:mm a")
+        LocalDateTime createdAt
+) {
 }
--- src/main/java/com/terxiel/store/mappers/UserMapper.java
+++ src/main/java/com/terxiel/store/mappers/UserMapper.java
@@ -3,8 +3,10 @@ package com.terxiel.store.mappers;
 import com.terxiel.store.dtos.UserSummary;
 import com.terxiel.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserSummary toDto(User user);
 }
