64486b77de446eac9620f0214a5cd24d57c26b66
terfiel-22 <dietherfiel@gmail.com>
2026-06-27 22:26:31 +0800

    minor update

--- src/main/java/com/terxiel/store/controllers/UserController.java
+++ src/main/java/com/terxiel/store/controllers/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Objects;
 import java.util.Set;
 
 @RestController
