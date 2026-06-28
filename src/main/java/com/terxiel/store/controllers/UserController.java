e13c806533f1cffb5132e142328d5cf33b452304
terfiel-22 <dietherfiel@gmail.com>
2026-06-28 13:40:38 +0800

    Add Validation and handling validation errors on User Registration Request.

--- src/main/java/com/terxiel/store/controllers/UserController.java
+++ src/main/java/com/terxiel/store/controllers/UserController.java
@@ -6,13 +6,17 @@ import com.terxiel.store.dtos.UpdateUserRequest;
 import com.terxiel.store.dtos.UserSummary;
 import com.terxiel.store.mappers.UserMapper;
 import com.terxiel.store.repositories.UserRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -50,7 +54,7 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserSummary> createUser(
-            @RequestBody RegisterUserRequest registerUserRequest,
+            @Valid @RequestBody RegisterUserRequest registerUserRequest,
             UriComponentsBuilder uriComponentsBuilder
     )
     {
@@ -119,5 +123,14 @@ public class UserController {
         userRepository.save(user);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationErrors(MethodArgumentNotValidException exception)
+    {
+        var errors = new HashMap<String,String>();
+        exception.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(),error.getDefaultMessage()));
+
+        return ResponseEntity.unprocessableContent().body(errors);
     }
 }
