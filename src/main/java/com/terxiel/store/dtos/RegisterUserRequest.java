836744185bed2bf53d27cd8268babee1c0065bfe
terfiel-22 <dietherfiel@gmail.com>
2026-06-28 14:14:24 +0800

    Implements custom validation

--- src/main/java/com/terxiel/store/dtos/RegisterUserRequest.java
+++ src/main/java/com/terxiel/store/dtos/RegisterUserRequest.java
@@ -1,5 +1,6 @@
 package com.terxiel.store.dtos;
 
+import com.terxiel.store.validations.Lowercase;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -15,6 +16,7 @@ public record RegisterUserRequest(
 
         @NotBlank(message = "Email is required.")
         @Email(message = "Email must be valid.")
+        @Lowercase(message = "Email must be in lowercase.")
         String email,
 
         @NotBlank(message = "Password is required.")
