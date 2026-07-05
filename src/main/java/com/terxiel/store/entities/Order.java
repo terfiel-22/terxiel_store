4762b6a20b80d5d222b65ec7b25a7e5fcbc4811a
terfiel-22 <dietherfiel@gmail.com>
2026-07-05 23:02:25 +0800

    Create entities for Order and OrderItem

---
+++ src/main/java/com/terxiel/store/entities/Order.java
@@ -0,0 +1,40 @@
+package com.terxiel.store.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private User customer;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private LocalDate createdAt;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @OneToMany(mappedBy = "order")
+    private Set<OrderItem> orderItems = new LinkedHashSet<>();
+}
---
+++ src/main/java/com/terxiel/store/entities/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.terxiel.store.entities;
+
+public enum OrderStatus {
+    PENDING,
+    PAID,
+    FAILED,
+    CANCELLED
+}
